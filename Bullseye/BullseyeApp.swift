0ea4c2cb91d089bdce585c4dcff83e0dd4ca7072
joel alcantara burgos <alcantara0729@gmail.com>
2021-12-16 15:59:57 -0400

    Initial Commit

---
+++ Bullseye/BullseyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullseyeApp.swift
+//  Bullseye
+//
+//  Created by joel Alcantara on 16/12/21.
+//
+
+import SwiftUI
+
+@main
+struct BullseyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
